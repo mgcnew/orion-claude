9e9dd05ca8b218b7236df1e2607108dd2218b870
revoltos <mgc.info.new@gmail.com>
2026-05-14 10:29:21 -0300

    fix(sidebar): logo aumentada para 200px

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -43,7 +43,7 @@ function Logo({ collapsed, theme }) {
     <img
       src={logoLanding}
       alt="SR Gestão de Documentos"
-      style={{ height: 170, width: '100%', maxWidth: 230, objectFit: 'contain', objectPosition: 'center center', margin: '-38px 0 -48px' }}
+      style={{ height: 200, width: '100%', maxWidth: 230, objectFit: 'contain', objectPosition: 'center center', margin: '-48px 0 -58px' }}
     />
   );
 }
